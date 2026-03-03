d1347fb902e58cff1a6dd4188a380a3aaea37429
lewis-ship-it <lewiswambirigatune@gmail.com>
2026-03-03 13:39:04 +0300

    Fix AgentState structure

--- alignment_core/world_model/__init__.py
+++ alignment_core/world_model/__init__.py
@@ -1,11 +1,5 @@
-# alignment_core/world_model/__init__.py
-
 from .world_state import WorldState
 from .agent import AgentState
 from .environment import EnvironmentState
 
-__all__ = [
-    "WorldState",
-    "AgentState",
-    "EnvironmentState",
-]
+__all__ = ["WorldState", "AgentState", "EnvironmentState"]
